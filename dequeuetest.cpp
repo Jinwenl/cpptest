cc0246ca2dbf0eb5327b4950fd7530ea58d3230f
jinwenl <jl43@njit.edu>
2012-12-24 11:29:00 -0500

    add

--- dequeuetest.cpp
+++ dequeuetest.cpp
@@ -13,5 +13,12 @@ int main(){
 	d.push_front(2);
 	cout << d.size() << endl << d[0] << d[1] << endl;
 
+	queue<int>q;
+	q.push(1);
+	q.push(2);
+	q.push(3);
+//	cout << q[0] << q[1] << q[2] << endl;	
+	cout << q.back() << endl;
+
 	return 0;
 }
