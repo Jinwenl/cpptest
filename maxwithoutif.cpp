250341554584be68bdbee541762dfbab6731270a
jinwenl <jl43@njit.edu>
2013-01-05 15:01:22 -0500

    max without if

--- maxwithoutif.cpp
+++ maxwithoutif.cpp
@@ -4,14 +4,15 @@ using namespace std;
 
 int getMax(int a, int b) {
 	int c = a - b;
-	int k = (c >> 31) & 0x1;
+	//int k = (c >> 31) & 0x1;
+	int k = bool(c);
 	int max = a - k * c;
 	return max;
 }
 
 int main(){
-	int a = 3, b = 5;
-	cout << getMax(3,5) << endl;
+	int a = 5, b = 3;
+	cout << getMax(a,b) << endl;
 
 	return 0;
 }
