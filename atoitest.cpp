088bb1badb31a19873ab59ae22ca097891e917ff
jinwenl <jl43@njit.edu>
2013-01-19 04:50:52 -0500

    my atoi

--- atoitest.cpp
+++ atoitest.cpp
@@ -1,22 +1,46 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
 int my_atoi(char *s){
 	int k = 0;
+	int neg = 0;
+
+	while(*s == ' ')	// handle start at space situation
+		s++;
+
+	if(*s=='-'){		// handle negative
+		neg = 1;
+		s++;
+	}
+
 	while(*s){
+		if(*s < '0' || *s > '9')	
+			break;
+
 		k = (k<<3) + (k<<1) + *s - '0';
 		s++;
-	}	
+
+		if(k<0)
+			cout << "buffer overflow" << endl;
+	}
+	
+	if(neg == 1)
+		k = -k;
 	return k;
 }
 
 int main(){
-	char c[] = "3810";
+	char c[] = "  -2147483648s";
+	//char c[] = "  -100s3";
 	int i;
 	i = my_atoi(c);
 	cout << i << endl;
-	cout << (i + 1111) << endl;
+	cout <<atoi(c) << endl;
+
+//	int j = -2147483649;
+//	cout << j << endl;
 
 	return 0;
 
